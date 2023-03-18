1a1764e08fa40b9b51e848dd93c8f73728683437
Julsapargi Nursam <egifcb@gmail.com>
2023-03-18 08:59:00 +0700

    Initial Commit

---
+++ MovieHub/MovieHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieHubApp.swift
+//  MovieHub
+//
+//  Created by Julsapargi Nursam on 18/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
